05fda2f3052264fd7a8e4b5bb2e5e72ee44a76ac
Johan Lokna <jlokna@student-net.ethz.ch>
2022-05-16 13:05:26 +0200

    .

---
+++ project3Lib/UNet/max_classifier.py
@@ -0,0 +1,24 @@
+import torch
+from torch import nn
+
+class MaxClassifier(nn.Module):
+
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+        self.threshold = None
+
+    def fit(self, sample, label):
+        thresholds = torch.arange(0.5, 1, 0.025).to(device=sample.device)
+        scores = torch.empty_like(thresholds)
+
+        for i in range(torch.numel(thresholds)):
+            self.threshold = thresholds[i]
+            scores[i] = torch.count_nonzero(self(sample) == label)
+
+        self.threshold = torch.mean(thresholds[scores == scores.max()])
+    
+    def __call__(self, x):
+        pred = self.model(x)
+        mask = (pred > self.threshold).view((pred.shape[0], -1))
+        return torch.any(mask, dim=1)
