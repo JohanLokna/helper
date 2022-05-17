eadebc50da60d73480c46dae807e0e9dcfa5caba
Johan Lokna <jlokna@student-net.ethz.ch>
2022-05-17 09:36:38 +0200

    .

--- project3Lib/__init__.py
+++ project3Lib/__init__.py
@@ -1,4 +1,4 @@
-from .UNet import UNet, MaxClassifier
+from .UNet import UNet, MaxClassifier, gradcam_unet, integrad_unet, shap_unet
 from .transforms import *
 from .VAE import *
 from .utils import *
